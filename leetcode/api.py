c2d63f27e608fc1972c2931dc7175b7c2de9d7b3
theopin <theopinto98@gmail.com>
2023-07-11 18:11:16 +0800

    Add filter to filter questions by difficulty level

--- leetcode/api.py
+++ leetcode/api.py
@@ -18,7 +18,7 @@ def obtain_problem_json():
 
 
  # Creates a list to store questions partial information retrieved from API
-def synthesize_problem_json(problem_json, isToSort=True):
+def synthesize_problem_json(problem_json, params, isToSort=True):
     synthesized_problems = []
     for child in problem_json["stat_status_pairs"]:
 
@@ -27,16 +27,19 @@ def synthesize_problem_json(problem_json, isToSort=True):
             question_api_data = {
                 "id": child["stat"]["question_id"],
                 "title": child["stat"]["question__title"],
-                "difficulty_index": child["difficulty"]["level"],
+                "difficulty": child["difficulty"]["level"],
                 "url": LEETCODE_URL_BASE + child["stat"]["question__title_slug"],
                 "total_acs": child["stat"]["total_acs"]
             }
 
             synthesized_problems.append(question_api_data)
-    
+
+    if params["difficulty"]:
+        synthesized_problems = [e for e in synthesized_problems if e["difficulty"] == params["difficulty"]]
+
     if isToSort:
         # Sort by difficulty follwed by problem id in ascending order
-        synthesized_problems = sorted(synthesized_problems, key=lambda x: (x["difficulty_index"], x["id"]))
+        synthesized_problems = sorted(synthesized_problems, key=lambda x: (x["id"]))
 
     return synthesized_problems
 
--- leetcode/process.py
+++ leetcode/process.py
@@ -12,23 +12,26 @@ def run_scraper(params):
     scraper.create_web_driver()
 
     problem_json = api.obtain_problem_json()
-    synthesized_problems = api.synthesize_problem_json(problem_json)    
+    synthesized_problems = api.synthesize_problem_json(problem_json, params)   
 
     scraped_data_list = []
 
     upper_limit = min(params["index"] + params["quantity"] , len(synthesized_problems))
     for i in range(params["index"] , upper_limit):
-            scraped_data = scraper.scrape_question(i, synthesized_problems[i])
+            try:
+                scraped_data = scraper.scrape_question(i, synthesized_problems[i])
 
-                        
-            if (i != params["index"]) and ((i - params["index"]) % 30 == 0):
-                print(f"30 Questions scraped! Sleeping 20s\n")
-                time.sleep(20)
-            else:
-                print(f"Sleeping 1 secs\n")
-                time.sleep(1)
-                
-            scraped_data_list.append(scraped_data)
+                            
+                if (i != params["index"]) and ((i - params["index"]) % 30 == 0):
+                    print(f"30 Questions scraped! Sleeping 20s\n")
+                    time.sleep(20)
+                else:
+                    print(f"Sleeping 1 secs\n")
+                    time.sleep(1)
+                    
+                scraped_data_list.append(scraped_data)
+            except Exception:
+                raise Exception("Unable to scrape question", 502)
 
     response["data"] = {
                 "questions": scraped_data_list,
@@ -44,6 +47,7 @@ def process_params(params):
 
     new_params["index"] = params.get("index", default=None, type=int)
     new_params["quantity"] = params.get("quantity", default=None, type=int)
+    new_params["difficulty"] = params.get("difficulty", default=0, type=int)
 
     if not (new_params["index"] and (new_params["quantity"])):
         raise Exception("Invalid index and quantity specified", 400)
