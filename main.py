ee3b8ed287c4f5c35e9124beca33d3f3175219bc
theopin <theopinto98@gmail.com>
2023-07-11 12:38:25 +0800

    Refactor error handling structure

--- main.py
+++ main.py
@@ -21,54 +21,49 @@ PORT = os.getenv("PORT") or 3000
 
 @scraper_app.route("/", methods = ['GET']) 
 def run_scrape_operation():
-    
-    
-    question_start_index = int(request.headers["index"])
-    question_quantity = int(request.headers["quantity"])
-
-
-    scraper.create_web_driver()
-
-    problem_json = api.obtain_problem_json()
-    synthesized_problems = api.synthesize_problem_json(problem_json, LEETCODE_URL)    
-
-    scraped_data_list = []
-
-    upper_limit = min(question_start_index + question_quantity, len(synthesized_problems))
-    for i in range(question_start_index, upper_limit):
-        try:
-            scraped_data = scraper.scrape_question(i, synthesized_problems[i])
-
-                    
-            # Sleep for 25 for each problem and 30s after every 30 problems
-            if (i != question_start_index) and ((i - question_start_index) % 30 == 0):
-                print(f"30 Questions scraped! Sleeping 20s\n")
-                time.sleep(20)
-            else:
-                print(f"Sleeping 1 secs\n")
-                time.sleep(1)
-            
-            scraped_data_list.append(scraped_data)
-        
-        except Exception as e:
-            scraper.quit_web_driver()
-            print(f"Failed to extract problem! ")
-            
-            return {
-                "status" : "error",
-                "message" : f"{e}"
-            }
-
-    scraper.quit_web_driver()
-
-    return {
-        "status": "success",
-        "data": {
-            "questions": scraped_data_list,
-            "count": len(scraped_data_list)
+    response = {
+            "status" : "success",
         }
-    }
+    try:   
     
+        question_start_index = int(request.headers["index"])
+        question_quantity = int(request.headers["quantity"])
 
+
+        scraper.create_web_driver()
+
+        problem_json = api.obtain_problem_json()
+        synthesized_problems = api.synthesize_problem_json(problem_json, LEETCODE_URL)    
+
+        scraped_data_list = []
+
+        upper_limit = min(question_start_index + question_quantity, len(synthesized_problems))
+        for i in range(question_start_index, upper_limit):
+
+                scraped_data = scraper.scrape_question(i, synthesized_problems[i])
+
+                        
+                # Sleep for 25 for each problem and 30s after every 30 problems
+                if (i != question_start_index) and ((i - question_start_index) % 30 == 0):
+                    print(f"30 Questions scraped! Sleeping 20s\n")
+                    time.sleep(20)
+                else:
+                    print(f"Sleeping 1 secs\n")
+                    time.sleep(1)
+                
+                scraped_data_list.append(scraped_data)
+
+        response["data"] = {
+                "questions": scraped_data_list,
+                "count": len(scraped_data_list)
+        }
+    except Exception as e:
+        response["status"] = "error"
+        response["message"] = f"{e}"
+
+    finally:
+        scraper.quit_web_driver()
+        return response
+         
 
 scraper_app.run(port=PORT)
